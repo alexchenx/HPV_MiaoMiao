21c2af5842f726c08e10920c42573c1409b4a086
alexchenx <chenxie@aliyun.com>
2020-12-07 17:16:35 +0800

    小优化

--- HPV_MiaoMiao.py
+++ HPV_MiaoMiao.py
@@ -20,17 +20,23 @@ def QiangYiMiao():
 
     logger("启动微信")
     device.app_start("com.tencent.mm")  # 启动微信
-    time.sleep(2)  # 休息2秒，等待启动
+    time.sleep(5)  # 休息3秒，等待启动
 
-    screen_text_list = ["发现", "小程序", "秒苗"]
-    for t in screen_text_list:
-        logger(f"点击 {t} ")
-        device(text=t).click()
-        time.sleep(0.5)
+    logger("点击 发现")
+    device(text="发现").click()
+    time.sleep(1)
+
+    logger("点击 小程序")
+    device(text="小程序").click()
+    time.sleep(1)
+
+    logger("点击 秒苗")
+    device(text="秒苗").click()
+    time.sleep(5)
 
     logger("点击首页")
     device.click(270.0, 2142.0)  # 点击 首页
-    time.sleep(1)  # 休息1秒，等待页面反应过来
+    time.sleep(2)  # 休息1秒，等待页面反应过来
 
     logger("点击进入抢购页面")
     device.click(890.0, 862.0)
